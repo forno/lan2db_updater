aeed918a187bc8eb732aa4b0104614d13ff08798
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-04-27 13:08:31 +0900

    Add input test

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ lan2db_updater: main.cpp
 	clang++ -std=c++14 -I/usr/local/include/cppconn main.cpp -o lan2db_updater -lmysqlcppconn
 
 install:  all
-	install -m 710 lan2db_updater /usr/local/bin/
+	install -m 711 lan2db_updater /usr/local/bin/
 
 uninstall:
 	rm /usr/local/bin/lan2db_updater
--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <fstream>
 #include <iostream>
 
 int main(int argc, char** argv)
@@ -5,5 +6,12 @@ int main(int argc, char** argv)
   std::ios_base::sync_with_stdio(false);
   std::cin.tie(nullptr);
 
-  return 0;
+  std::string s;
+  std::getline(std::cin, s);
+  std::ofstream fs {"/tmp/syslog_develop.log"};
+  if (!fs) {
+    std::cerr << "can't open" << std::endl;
+    return 1;
+  }
+  fs << s << '\n';
 }
